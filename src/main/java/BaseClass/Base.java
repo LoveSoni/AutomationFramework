bdeae34f6077cf8a8a92409e18675a53719e0b45
love <love.1@cars24.com>
2019-10-13 22:10:53 +0530

    Added utility and Property Reader

--- src/main/java/BaseClass/Base.java
+++ src/main/java/BaseClass/Base.java
@@ -2,25 +2,28 @@ package BaseClass;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Base {
-    public AppiumDriver driver;
-    public DesiredCapabilities desiredCapabilities;
+    public AppiumDriver appDriver;
+    public WebDriver webDriver;
 
-    public void createSession() {
+    public void startSession() {
 
     }
 
     public void serverCapabilities() {
-
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
     }
 
     public void androidCapabilities() {
-        desiredCapabilities = new DesiredCapabilities();
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UIAutomator2");
     }
 
     public void iosCapabilities() {
-        desiredCapabilities = new DesiredCapabilities();
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"XCUITest");
     }
 }
---
+++ src/main/java/Utils/PropertyReader.java
@@ -0,0 +1,28 @@
+package Utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.io.FileNotFoundException;
+
+public class PropertyReader {
+    public FileInputStream fileInputStream;
+    public Properties properties;
+
+    public String getValue(String key, String filePath) {
+        String value = null;
+        try {
+            fileInputStream = new FileInputStream(filePath);
+        } catch (FileNotFoundException fe) {
+
+        }
+        properties = new Properties();
+        try {
+            properties.load(fileInputStream);
+        } catch (IOException ie) {
+
+        }
+        value = properties.getProperty(key);
+        return value;
+    }
+}
