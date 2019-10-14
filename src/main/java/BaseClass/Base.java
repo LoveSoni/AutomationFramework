d4961acd29121a13a6891638b25662b8a109dfe2
love <love.1@cars24.com>
2019-10-14 23:26:31 +0530

    Added serviceBuilder

--- src/main/java/BaseClass/Base.java
+++ src/main/java/BaseClass/Base.java
@@ -2,15 +2,22 @@ package BaseClass;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Base {
     public AppiumDriver appDriver;
     public WebDriver webDriver;
+    public AppiumServiceBuilder appiumServiceBuilder;
+    public AppiumDriverLocalService appiumDriverLocalService;
 
     public void startSession() {
-
+        appiumServiceBuilder = new AppiumServiceBuilder();
+        appiumServiceBuilder.usingAnyFreePort();
+        appiumServiceBuilder.withArgument(GeneralServerFlag.LOG_LEVEL,"info");
     }
 
     public void serverCapabilities() {
