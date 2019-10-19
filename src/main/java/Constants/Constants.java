480a1c9817672e349858acfc1a3a49f9f3e8d21c
love <love.1@cars24.com>
2019-10-19 16:27:28 +0530

    Path for apk

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -1,7 +1,12 @@
 package Constants;
 
+import com.sun.tools.internal.jxc.ap.Const;
+
 import java.io.File;
 
 public class Constants {
     public String slash = File.separator;
+
+    public String DEALER_APP_PATH = "Users" + slash + System.getProperty("user.name") + slash + "documents" + slash + "builds" + "catalogv2.apk";
+
 }
---
+++ src/test/java/parallelTests/runParallel.java
@@ -0,0 +1,22 @@
+package parallelTests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class runParallel {
+    @BeforeMethod
+    public void setUp() {
+
+    }
+
+    @Test
+    public void login() {
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+    }
+}
