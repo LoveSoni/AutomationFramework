4a20f39bedee72a1fc1c8714ebd4ebea7894210e
Love <lovesoni607@gmail.com>
2019-10-30 10:17:31 +0530

    parallel commit

--- src/main/java/Utils/AdbUtility.java
+++ src/main/java/Utils/AdbUtility.java
@@ -1,4 +1,23 @@
 package Utils;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 public class AdbUtility {
+    Logger logger = Logger.getLogger(AdbUtility.class);
+
+    public String getReferenceImageB64(String imgPath) throws IOException {
+        byte[] fileContent = FileUtils.readFileToByteArray(new File(imgPath));
+        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+        return encodedString;
+    }
 }
--- src/test/java/parallelTests/runParallel.java
+++ src/test/java/parallelTests/runParallel.java
@@ -1,27 +1,29 @@
 package parallelTests;
 
 import BaseClass.Base;
+import Utils.AdbUtility;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.Setting;
+import io.appium.java_client.android.AndroidDriver;
+import org.kushan.appium.locator.AppiumLocator;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class runParallel extends Base {
 
-    runParallel()
-    {
-        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
-    }
-    @AndroidFindBy(xpath = "//android.widget.TextView[@text='NEXT']")
-    public MobileElement nextButton;
+//    runParallel()
+//    {
+//        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
+//    }
+//    @AndroidFindBy(xpath = "//android.widget.TextView[@text='NEXT']")
+//    public MobileElement nextButton;
 
     @Parameters({"udid"})
     @BeforeMethod
@@ -30,15 +32,22 @@ public class runParallel extends Base {
     }
 
     @Test
-   synchronized public void login() {
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.DAYS.SECONDS);
-        nextButton.click();
-        nextButton.click();
-        driver.findElement(By.xpath("//android.widget.TextView[@text='GET STARTED']")).click();
-        driver.findElementByAccessibilityId("login_via_username_btn").click();
-        driver.findElementByAccessibilityId("enter_username").sendKeys("testdealer@cars24.com");
-        driver.findElementByAccessibilityId("enter_password").sendKeys("123456");
-        driver.findElementByAccessibilityId("login_btn").click();
+    synchronized public void login() throws IOException {
+        AdbUtility adbUtility = new AdbUtility();
+        String imagePath = System.getProperty("user.dir") + File.separator + "login.png";
+        AppiumLocator.GetAppiumLocators();
+//        logger.info("Image path is :" + imagePath);
+//        driver.manage().timeouts().implicitlyWait(60, TimeUnit.DAYS.SECONDS);
+//        driver.findElement(By.xpath("//android.widget.TextView[@text='NEXT']")).click();
+//        driver.findElement(By.xpath("//android.widget.TextView[@text='NEXT']")).click();
+//        driver.findElement(By.xpath("//android.widget.TextView[@text='GET STARTED']")).click();
+//        MobileElement loginButton = (MobileElement) driver.findElementByImage(adbUtility.getReferenceImageB64(imagePath));
+//        ((AndroidDriver) driver).setSetting(Setting.UPDATE_IMAGE_ELEMENT_POSITION, true);
+//        loginButton.click();
+//        driver.findElementByAccessibilityId("login_via_username_btn").click();
+//        driver.findElementByAccessibilityId("enter_username").sendKeys("testdealer@cars24.com");
+//        driver.findElementByAccessibilityId("enter_password").sendKeys("123456");
+//        driver.findElementByAccessibilityId("login_btn").click();
     }
 
     @AfterMethod
