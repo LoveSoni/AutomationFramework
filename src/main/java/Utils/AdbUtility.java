24378db88ac71e8dede29c25e10c7be849ade3dc
love <lovesoni607@gmail.com>
2019-11-02 17:26:41 +0530

    adb utilility to get list of devices

--- src/main/java/Utils/AdbUtility.java
+++ src/main/java/Utils/AdbUtility.java
@@ -1,23 +1,24 @@
 package Utils;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.support.PageFactory;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Base64;
+import java.io.InputStreamReader;
 
 public class AdbUtility {
     Logger logger = Logger.getLogger(AdbUtility.class);
 
-    public String getReferenceImageB64(String imgPath) throws IOException {
-        byte[] fileContent = FileUtils.readFileToByteArray(new File(imgPath));
-        String encodedString = Base64.getEncoder().encodeToString(fileContent);
-        return encodedString;
+    public void getListOfDevices(String[] args) {
+        try {
+            String line;
+            Process p = Runtime.getRuntime().exec("adb devices");
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                logger.info(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/Utils/CommonUtility.java
+++ src/main/java/Utils/CommonUtility.java
@@ -1,4 +1,15 @@
 package Utils;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
 public class CommonUtility {
+    public String getReferenceImageB64(String imgPath) throws IOException {
+        byte[] fileContent = FileUtils.readFileToByteArray(new File(imgPath));
+        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+        return encodedString;
+    }
 }
