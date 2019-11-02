034724c571a6cd21c6eb9bc7727ef4dbc761150b
love <lovesoni607@gmail.com>
2019-11-02 16:12:42 +0530

    utility to update jsonObject from jsonFile

--- src/main/java/Utils/JsonReader.java
+++ src/main/java/Utils/JsonReader.java
@@ -1,37 +1,47 @@
 package Utils;
 
+import com.google.gson.JsonParser;
 import org.apache.log4j.Logger;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class JsonReader {
-
-    public JSONParser jsonParser;
     public Logger logger = Logger.getLogger(JsonReader.class);
     public JSONObject jsonObject;
 
     public String readJsonValue(String jsonFilePath, String key) {
-        String value = null;
-        jsonParser = new JSONParser();
+        JSONObject jsonValue;
+        jsonValue = jsonFileReader(jsonFilePath);
+        return String.valueOf(jsonValue.get(key));
+    }
+
+    public JSONObject jsonFileReader(String jsonFilePath) {
+        JSONParser jsonParser = new JSONParser();
         try (FileReader fileReader = new FileReader(jsonFilePath)) {
             jsonParser = new JSONParser();
             try {
                 Object object = jsonParser.parse(fileReader);
                 jsonObject = (JSONObject) object;
-                value = jsonObject.get(key).toString();
             } catch (ParseException e) {
                 logger.info(e.getMessage());
             }
         } catch (IOException e) {
             logger.info(e.getMessage());
         }
-        return value;
+        return jsonObject;
+    }
+
+    public JSONObject updateJsonObject(String jsonFilePath, String key, String newValue) {
+        JSONObject replaceJsonObject;
+        replaceJsonObject = jsonFileReader(jsonFilePath);
+        replaceJsonObject.replace(key, newValue);
+        return replaceJsonObject;
     }
 }
