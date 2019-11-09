672a3347847f0945e4a27bbf0ec2426cccafd99c
love <lovesoni607@gmail.com>
2019-11-09 15:26:51 +0530

    Utility to replace value in properties fiel

--- src/main/java/Utils/PropertyReader.java
+++ src/main/java/Utils/PropertyReader.java
@@ -1,28 +1,49 @@
 package Utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
-import java.io.FileNotFoundException;
 
 public class PropertyReader {
     public FileInputStream fileInputStream;
-    public Properties properties;
+    public FileOutputStream fileOutputStream;
 
     public String getValue(String key, String filePath) {
         String value = null;
+        Properties properties = readProperties(filePath);
+        value = properties.getProperty(key);
+        return value;
+    }
+
+    public Properties readProperties(String filePath) {
+        Properties properties = new Properties();
         try {
             fileInputStream = new FileInputStream(filePath);
         } catch (FileNotFoundException fe) {
             fe.printStackTrace();
         }
-        properties = new Properties();
         try {
             properties.load(fileInputStream);
+            fileInputStream.close();
         } catch (IOException ie) {
             ie.printStackTrace();
         }
-        value = properties.getProperty(key);
-        return value;
+        return properties;
     }
+
+    public void replaceValue(String key, String value, String filePath) {
+        Properties properties = readProperties(filePath);
+        try {
+            fileOutputStream = new FileOutputStream(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        properties.replace(key, value);
+        try {
+            properties.store(fileOutputStream, "");
+            fileOutputStream.close();
+        } catch (IOException iE) {
+            iE.printStackTrace();
+        }
+    }
+
 }
