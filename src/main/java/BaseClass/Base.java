8c9a664b3f5f0e09f2694586c680c35f3c99a954
love <love.1@cars24.com>
2020-01-13 15:19:15 +0530

    Redesign base class

--- src/main/java/BaseClass/Base.java
+++ src/main/java/BaseClass/Base.java
@@ -1,66 +1,58 @@
 package BaseClass;
+/**
+ * author Love
+ */
 
 import Constants.Constants;
-import Utils.PropertyReader;
-
 import java.net.URL;
-
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.io.File;
 
 public class Base {
-    public AppiumDriver driver;
-    public WebDriver webDriver;
-    public Logger logger = Logger.getLogger(Base.class);
     public AppiumServiceBuilder appiumServiceBuilder;
     public AppiumDriverLocalService appiumDriverLocalService;
-    public Constants constants = new Constants();
-    public PropertyReader propertyReader = new PropertyReader();
 
     public void startSession() {
         appiumServiceBuilder = new AppiumServiceBuilder();
         appiumServiceBuilder.usingAnyFreePort();
-        appiumServiceBuilder.withIPAddress(constants.APPIUM_DEFAULT_IP);
+        appiumServiceBuilder.withIPAddress(Constants.APPIUM_DEFAULT_IP);
         appiumServiceBuilder.withArgument(GeneralServerFlag.LOG_LEVEL, "warn");
         appiumDriverLocalService = appiumDriverLocalService.buildService(appiumServiceBuilder);
         appiumDriverLocalService.start();
-    }
-
-    public void initDriver(String udid) {
-        startSession();
-        String configFilePath = System.getProperty("user.dir") + constants.slash + "src" + constants.slash + "main" + constants.slash + "resources" + File.separator + "config.properties";
-        String capabilities = propertyReader.getValue("platformName", configFilePath);
-        driver = new AndroidDriver(getAppiumURL(), clientCapabilities(capabilities, udid));
-    }
-
-    public URL getAppiumURL() {
-        return appiumDriverLocalService.getUrl();
     }
 
     public void stopSession() {
         appiumDriverLocalService.stop();
     }
 
-    public DesiredCapabilities clientCapabilities(String typeCapabilities, String udid) {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator");
-        logger.info("Tests running on udid " + udid);
-        if (typeCapabilities.equalsIgnoreCase("android")) {
-            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
-            desiredCapabilities.setCapability(MobileCapabilityType.APP, constants.DEALER_APP_PATH);
-            desiredCapabilities.setCapability(MobileCapabilityType.UDID, udid);
-        } else {
-            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
-        }
-        return desiredCapabilities;
+    public URL getAppiumServiceURL() {
+        return appiumDriverLocalService.getUrl();
     }
+
+//    public void initDriver(String udid) {
+//        startSession();
+//        String configFilePath = System.getProperty("user.dir") + constants.slash + "src" + constants.slash + "main" + constants.slash + "resources" + File.separator + "config.properties";
+//        String capabilities = propertyReader.getValue("platformName", configFilePath);
+//        driver = new AndroidDriver(getAppiumURL(), clientCapabilities(capabilities, udid));
+//    }
+
+
+
+
+
+//    public DesiredCapabilities clientCapabilities(String typeCapabilities, String udid) {
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator");
+//        logger.info("Tests running on udid " + udid);
+//        if (typeCapabilities.equalsIgnoreCase("android")) {
+//            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+//            desiredCapabilities.setCapability(MobileCapabilityType.APP, constants.DEALER_APP_PATH);
+//            desiredCapabilities.setCapability(MobileCapabilityType.UDID, udid);
+//        } else {
+//            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+//        }
+//        return desiredCapabilities;
+//    }
+
 }
--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -4,20 +4,20 @@ import java.io.File;
 
 public class Constants {
 
-    public final String slash = File.separator;
+    public static final String slash = File.separator;
 
-    public final String DEALER_APP_PATH = slash + "Users" + slash + System.getProperty("user.name") + slash + "documents" + slash + "builds" + slash + "catalogv2.apk";
+    public static final String DEALER_APP_PATH = slash + "Users" + slash + System.getProperty("user.name") + slash + "documents" + slash + "builds" + slash + "catalogv2.apk";
 
-    public final String APPIUM_DEFAULT_IP = "127.0.0.1";
+    public static final String APPIUM_DEFAULT_IP = "127.0.0.1";
 
-    public final String PROJ_PATH = System.getProperty("user.dir");
+    public static final String PROJ_PATH = System.getProperty("user.dir");
 
-    public final String RESOURCES_PROP_PATH = PROJ_PATH + slash + "src" + slash + "main" + slash + "resources";
+    public static final String RESOURCES_PROP_PATH = PROJ_PATH + slash + "src" + slash + "main" + slash + "resources";
 
-    public final String SHELL_PROP_PATH = RESOURCES_PROP_PATH + slash + "shellCommand.properties";
+    public static final String SHELL_PROP_PATH = RESOURCES_PROP_PATH + slash + "shellCommand.properties";
 
-    public final String CONFIG_PROP_PATH = RESOURCES_PROP_PATH + slash + "config.properties";
+    public static final String CONFIG_PROP_PATH = RESOURCES_PROP_PATH + slash + "config.properties";
 
-    public int WAIT_TIME = 10;
+    public static int WAIT_TIME = 10;
 
 }
