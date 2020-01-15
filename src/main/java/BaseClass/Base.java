449740bd9f41a706e739536690142ada4d5f1427
love <love.1@cars24.com>
2020-01-15 23:55:37 +0530

    check for session

--- src/main/java/BaseClass/Base.java
+++ src/main/java/BaseClass/Base.java
@@ -1,11 +1,21 @@
 package BaseClass;
 
+import Library.AndroidSession;
+import Library.IosSession;
 import Library.SessionManager;
+import Library.WebSession;
+import Utils.EnvironmentParameters;
 
 public class Base {
     private SessionManager sessionManager;
 
-    public Base(){
-
+    public Base() {
+        if (EnvironmentParameters.getPlatformOS().equalsIgnoreCase("android")) {
+            sessionManager = new AndroidSession();
+        } else if (EnvironmentParameters.getPlatformOS().equalsIgnoreCase("iOS")) {
+            sessionManager = new IosSession();
+        } else {
+            sessionManager = new WebSession();
+        }
     }
 }
