afb68d6c1ae370dd22d5a40e2cfd277e87f7acf1
love <love.1@cars24.com>
2020-01-23 15:16:33 +0530

    updated pom.xml

--- src/main/java/Utils/JavaUtility.java
+++ src/main/java/Utils/JavaUtility.java
@@ -10,7 +10,7 @@ import java.util.Base64;
  * author Love
  */
 
-public class CommonUtility {
+public class JavaUtility {
     public String getReferenceImageB64(String imgPath) throws IOException {
         byte[] fileContent = FileUtils.readFileToByteArray(new File(imgPath));
         String encodedString = Base64.getEncoder().encodeToString(fileContent);
