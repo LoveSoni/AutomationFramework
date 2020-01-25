01dc369a0cfe167bde2fcf019528d8d339f259e2
love <love.1@cars24.com>
2020-01-25 14:03:00 +0530

    fix excel util

--- src/main/java/Utils/ExcelUtility.java
+++ src/main/java/Utils/ExcelUtility.java
@@ -23,25 +23,19 @@ public class ExcelUtility {
             xssfWorkbook = new XSSFWorkbook(fileInputStream);
         } catch (IOException e) {
             //log exception here
+            System.out.print(e.getMessage());
         }
         xssfSheet = xssfWorkbook.getSheet(sheetName);
         Object[][] sheetData = new Object[xssfSheet.getLastRowNum()][xssfSheet.getRow(1).getLastCellNum()];
-        for (int i = 1; i < xssfSheet.getLastRowNum(); i++)
+        for (int i = 0; i < xssfSheet.getLastRowNum(); i++)
         {
             for(int j=0;j<xssfSheet.getRow(1).getLastCellNum();j++)
             {
-                sheetData[i][j] = xssfSheet.getRow(i).getCell(j).getStringCellValue();
+                sheetData[i][j] = xssfSheet.getRow(i+1).getCell(j).getStringCellValue();
                 System.out.println(sheetData[i][j]);
             }
             System.out.print("\n");
         }
         return sheetData;
-    }
-
-    public static void main(String args[])
-    {
-        String filePath = "/Users/love/Documents/AutomationFramework/src/main/java/Utils/Yes.xls";
-        ExcelUtility excelUtility = new ExcelUtility();
-        excelUtility.getExcelData(filePath,"Sheet1");
     }
 }
