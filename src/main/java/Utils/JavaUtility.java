c7bf1047520771a2ff18bf3eb73cc770d45344cc
love <lovesoni607@gmail.com>
2020-01-26 15:28:13 +0530

    date and time util

--- src/main/java/Utils/JavaUtility.java
+++ src/main/java/Utils/JavaUtility.java
@@ -1,21 +1,40 @@
 package Utils;
 
 import org.apache.commons.io.FileUtils;
+import sun.java2d.pipe.SpanShapeRenderer;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Base64;
+import java.util.Date;
 
 /**
  * author Love
  */
 
 public class JavaUtility {
-    public String getReferenceImageB64(String imgPath) throws IOException {
+    public static String getReferenceImageB64(String imgPath) throws IOException {
         byte[] fileContent = FileUtils.readFileToByteArray(new File(imgPath));
         String encodedString = Base64.getEncoder().encodeToString(fileContent);
         return encodedString;
     }
 
+    public static String getCurrentDateAndTime()
+    {
+        DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    public static String getCurrentDateAndTime(String pattern)
+    {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return dateTimeFormatter.format(localDateTime);
+    }
 
 }
